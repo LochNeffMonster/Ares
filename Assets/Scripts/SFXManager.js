5710f23591ee007d6eca335bdda823e6373e3266
LochNeffMonster <atneff@ucsc.edu>
2014-05-12 14:08:25 -0700

    Added ping sounds to the main menu

---
+++ Assets/Scripts/SFXManager.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+public var confirmPing : AudioClip;
+public var sfxSource : AudioSource;
+public var listener:AudioListener;
+public var listenerObject:GameObject;
+
+function Awake () {
+    DontDestroyOnLoad(gameObject);
+    sfxSource = gameObject.AddComponent(AudioSource);
+    confirmPing = Resources.Load("Audio/ConfirmPing") as AudioClip;
+    listener = FindObjectOfType(AudioListener);
+    listenerObject = listener.gameObject;
+    gameObject.transform.position = listenerObject.transform.position;
+}
+
+function Start () {
+    sfxSource.clip = confirmPing;
+    sfxSource.Play();
+
+}
+
+function PlayConfirmPing(){
+    audio.Play();
+}
+
+function RequestPing(){
+    PlayConfirmPing();
+}
+
+function OnLevelWasLoaded (level : int) {
+    listener = FindObjectOfType(AudioListener);
+    listenerObject = listener.gameObject;
+    gameObject.transform.position = listenerObject.transform.position;
+}
